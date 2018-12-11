5df2761fce947a0e2e07f6c7bc0a8252f2c74759
Anthony <anthonydivitto@gmail.com>
2018-12-11 04:55:29 -0700

    small code and style clean up

--- Source/UnlimitedEngine/Game/Game.lua
+++ Source/UnlimitedEngine/Game/Game.lua
@@ -1,45 +1,15 @@
 return {
     Textures = {
                     [0] = "DivittoUnlimitedLogo"
---                    ,     "Entities"
---                    ,     "Particle"
---                    ,     "Explosion"
---                    ,     "GreenTank"
---                    ,     "Helicopter"
---                    ,     "Ally"
---                    ,     "Tree"
---                    ,     "Fuel"
---                    ,     "HUDBackground"
---                    ,     "HelicopterBlades"
---                    ,     "Grass"
 };
     Fonts               = {
-                --      [0] = "Default"
     };
     Music               = {
---                    [0] = "MenuTheme"
---                    ,     "GameTheme"
---                    ,     "FuelLow"
     };
     SoundEffects        = {
---                    [0] = "Explosion"
---                    ,     "PlayerShoot"
---                    ,     "CollectPickup"
---                    ,     "EnemyGunfire"
---                    ,     "PlayerDeath"
---                    ,     "TroopCountFull"
---                    ,     "ReturnToBase"
---                    ,     "LetsGo"
---                    ,     "CollectAlly"
---                    ,     "FuelLow"
     };
     Particles = {
---                    [0] = "MissilePropellant"
---                    ,     "MissileSmoke"
---                    ,     "AircraftPropellant"
---                    ,     "AircraftSmoke"
---                    ,     "DamageSmoke"
     };
-    --PlayerTexture       = "PlayerAircraft";
+    PlayerTexture       = "DivittoUnlimitedLogo";
     PlayerTexture   = "DivittoUnlimitedLogo";
 };
