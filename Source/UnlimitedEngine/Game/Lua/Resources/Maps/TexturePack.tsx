4957e35b47c7991a36463805c769e0f8ea932d51
divitto <anthonydivitto@gmail.com>
2020-05-05 21:27:33 -0700

    Small Commit to merge files with laptop and update repo, just added some new tiles and the start of a new map.

---
+++ Source/UnlimitedEngine/Game/Lua/Resources/Maps/TexturePack.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="TexturePack" tilewidth="64" tileheight="64" tilecount="100" columns="10" backgroundcolor="#00000000">
+ <image source="../../../../Media/Textures/TexturePack.png" width="640" height="640"/>
+ <tile id="0" type="Ground"/>
+ <tile id="1" type="Ground"/>
+ <tile id="2" type="Road"/>
+ <tile id="3" type="Water"/>
+ <tile id="4" type="Wall"/>
+ <tile id="14" type="Tree"/>
+ <tile id="24" type="Tree"/>
+ <tile id="34" type="Tree"/>
+</tileset>
